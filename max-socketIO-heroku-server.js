3c11420d7f2aee412ee628b58230a1660ef13721
Fede Camara Halac <camarafede@gmail.com>
2020-05-19 15:57:38 -0400

    testing socket connection

--- max-socketIO-heroku-server.js
+++ max-socketIO-heroku-server.js
@@ -1,8 +1,3 @@
-/*
-    Node Server hosted on Heroku
-    by Eric Sheffield, Nick Hwang, and Anthony T. Marasco (2020)
-*/
-
 const myport=(process.env.PORT || 80);
 
 console.log(`defined port at: ${myport}`);
@@ -28,30 +23,32 @@ const io      = require('socket.io').listen(server);
 server.listen(myport); // start listening for socket connections
 
 
-// io.on('connection', (socket) => {
+console.log("begin socket connection...");
+
+io.on('connection', (socket) => {
     
-//     // --- intial connection messages
+    // --- intial connection messages
 
-//     // post new connection message to server console
-//     console.log('A client has connected.');
+    // post new connection message to server console
+    console.log('A client has connected.');
 
-//     // send a confirmation message to a new client when they have connected
-//     socket.emit('messageFromServer', "Welcome from the server!"); 
+    // send a confirmation message to a new client when they have connected
+    socket.emit('messageFromServer', "Welcome from the server!"); 
 
-//     // let the other clients know about the new particpant
-//     socket.broadcast.emit('messageFromServer', "Someone else has joined!");
+    // let the other clients know about the new particpant
+    socket.broadcast.emit('messageFromServer', "Someone else has joined!");
     
-//     // ---
+    // ---
 
-//     // --- client to client communications
+    // --- client to client communications
 
-//     // chat messages
-//     socket.on('chatToServer', (chat) => {
-//         socket.broadcast.emit('chatFromServer', chat);
-//     })
+    // chat messages
+    socket.on('chatToServer', (chat) => {
+        socket.broadcast.emit('chatFromServer', chat);
+    })
 
-//     // data
-//     socket.on('dataToServer', (data) => {
-//         socket.broadcast.emit('dataFromServer', data);
-//     })
-// });
+    // data
+    socket.on('dataToServer', (data) => {
+        socket.broadcast.emit('dataFromServer', data);
+    })
+});
