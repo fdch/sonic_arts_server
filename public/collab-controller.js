c05c9763eba1238cd47d5c6cf1e0cdf0c8b137de
Fede Camara Halac <camarafede@gmail.com>
2020-05-23 17:02:37 -0400

    changed browser client code

--- public/collab-controller.js
+++ public/collab-controller.js
@@ -1,64 +1,74 @@
-const io = require('socket.io');
+
   // messages
   $(function() { // called when DOM is ready
 
     // establishes a socket.io connection
-    var socket = io.connect('https://sonic-arts-server.herokuapp.com');
+    var socket = io();
+
+    socket.on('users', function(users) {
+      console.log("users!: " + users);
+    })
 
     // interface functions
-    $('#shootbutton1').click(function() {
-      socket.emit('inc', '1');
-      console.log("increaseP1");
+    $('#event-button1').click(function() {
+      socket.emit('event', 'webEvent1');
+      console.log("webevent1 triggered");
       return false; // false does not reload the page
     });
 
-    $('#inc-button1').click(function() {
-      socket.emit('inc', '1');
-      console.log("increaseP1");
+    $('#event-button2').click(function() {
+      socket.emit('event', 'webEvent2');
+      console.log("webevent2 triggered");
       return false; // false does not reload the page
     });
 
-    $('#inc-button2').click(function() {
-      socket.emit('inc', '2');
-      console.log("increaseP2");
+    $('#event-button3').click(function() {
+      socket.emit('event', 'webEvent3');
+      console.log("webevent3 triggered");
       return false; // false does not reload the page
     });
 
-    $('#inc-button3').click(function() {
-      socket.emit('inc', '3');
-      console.log("increaseP3");
+    $('#event-button4').click(function() {
+      socket.emit('event', 'webEvent4');
+      console.log("webevent4 triggered");
       return false; // false does not reload the page
     });
 
-    $('#inc-button4').click(function() {
-      socket.emit('inc', '4');
-      console.log("increaseP4");
+    //
+
+    $('#control-webSlider1').on('input', function() {
+      var val = $('#control-webSlider1').val();
+      $('#slider1-label').text(`control webSlider1 ${val}`);
+      socket.emit('control', {header: 'webSlider1', values: val});
+      console.log('control', {header: 'webSlider1', values: val});
       return false; // false does not reload the page
     });
 
-    $('#dec-button1').click(function() {
-      socket.emit('dec', '1');
-      console.log("decreaseP1");
+    $('#control-webSlider2').on('input', function() {
+      var val = $('#control-webSlider2').val();
+      $('#slider2-label').text(`control webSlider2 ${val}`);
+      socket.emit('control', {header: 'webSlider2', values: val});
+      console.log('control', {header: 'webSlider2', values: val});
       return false; // false does not reload the page
     });
 
-    $('#dec-button2').click(function() {
-      socket.emit('dec', '2');
-      console.log("decreaseP2");
+    $('#control-webSlider3').on('input', function() {
+      var val = $('#control-webSlider3').val();
+      $('#slider3-label').text(`control webSlider3 ${val}`);
+      socket.emit('control', {header: 'webSlider3', values: val});
+      console.log('control', {header: 'webSlider3', values: val});
       return false; // false does not reload the page
     });
 
-    $('#dec-button3').click(function() {
-      socket.emit('dec', '3');
-      console.log("decreaseP3");
+    $('#control-webSlider4').on('input', function() {
+      var val = $('#control-webSlider4').val();
+      $('#slider4-label').text(`control webSlider4 ${val}`);
+      socket.emit('control', {header: 'webSlider4', values: val});
+      console.log('control', {header: 'webSlider4', values: val});
       return false; // false does not reload the page
     });
 
-    $('#dec-button4').click(function() {
-      socket.emit('dec', '4');
-      console.log("decreaseP4");
-      return false; // false does not reload the page
-    });
+//
 
     $('#collectible-button').click(function() {
       socket.emit('spawnCollectible');
